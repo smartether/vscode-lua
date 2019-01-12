9199ea2afa6d8058b461e9e21d7422bea9d8475e
etherkillerQ <smartether@gmail.com>
2019-01-12 22:39:41 +0800

    1,error catch.

--- server/src/analysis/analysis.ts
+++ server/src/analysis/analysis.ts
@@ -221,44 +221,45 @@ export class Analysis {
 
         let comment = '';
 
-        if (node.type === 'FunctionDeclaration') {
-            const fun = node as luaparse.FunctionDeclaration;
-            const startLine = fun.loc.start.line;
-            let src = '';
-            src += luaparse.src.txt;
-            if (src == null || src === undefined) {
-                src = '';
-            }
+        // if (node.type === 'FunctionDeclaration') {
+        //     const fun = node as luaparse.FunctionDeclaration;
+        //     const startLine = fun.loc.start.line;
+        //     let src = '';
+        //     src += luaparse.src.txt;
+        //     if (src == null || src === undefined) {
+        //         src = '';
+        //     }
 
-            const lines = src.split('\n');
-            const lines1 = src.split('\r\n');
-            const lines2 = src.split('/\r/\n');
-            let codeline = lines;
-            if (lines1.length > codeline.length) {
-                codeline = lines1;
-            }
-            if (lines2.length > codeline.length) {
-                codeline = lines2;
-            }
+        //     const lines = src.split('\n');
+        //     const lines1 = src.split('\r\n');
+        //     const lines2 = src.split('/\r/\n');
+        //     let codeline = lines;
+        //     if (lines1.length > codeline.length) {
+        //         codeline = lines1;
+        //     }
+        //     if (lines2.length > codeline.length) {
+        //         codeline = lines2;
+        //     }
 
-            if (codeline != null) {
-                const commentRef = codeline.find((v, i) => v != null && i === startLine);
+        //     if (codeline != null) {
+        //         const commentRef = codeline.find((v, i) => v != null && i === startLine);
 
-                try {
-                    comment += commentRef;
-                } catch (error) {
+        //         try {
+        //             comment += commentRef;
+        //         } catch (error) {
 
-                }
-                try {
-                    comment += src;
-                } catch (error) {
+        //         }
+        //         try {
+        //             comment += src;
+        //         } catch (error) {
 
-                }
-            }
+        //         }
+        //     }
 
-        }
+        // }
 
-        comment += this.chunks.length.toString();
+        // comment += this.chunks.length.toString();
+        comment += luaparse.src.txt;
         this.symbols.push({
             kind,
             name,
